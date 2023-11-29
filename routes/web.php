f5cd79fd48b487288e6594b81ecceb8934130553
NetalSarda <netalsarda11@gmail.com>
2023-11-29 22:18:10 +0530

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
 Route::view("/", 'home')->name('home');
 Route::view("/about", 'about')->name('about');
 Route::view("/history", 'history')->name('tenure');
