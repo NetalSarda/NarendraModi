71e29a34a774348449b5dbd4d5eec6645243358d
NetalSarda <netalsarda11@gmail.com>
2023-11-29 22:01:50 +0530

    updated gitignore and readme.md

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,9 +1,7 @@
-<script src="https://cdn.tailwindcss.com"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Narendra modi Web Site</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-<div class="width-full h-full">
-<main class="w-full h-200">
-            @yield('appContents')
-        </main>
-        </div>
-</body>
+    <div class="container">
---
+++ resources/views/tenure.blade.php
@@ -0,0 +1,24 @@
+<h1 class="text-4xl font-bold">History of Narendra Modi</h1>
+    </header>
+
+    <!-- Main Content Section -->
+
+
+    <section class="container mx-auto mt-8 px-4">
+            <h2 class="text-2xl font-semibold mb-4"">Early Life</h2>
+            <p>
+               Modi completed his higher secondary education in Vadnagar in 1967; his teachers described him as an average student and a keen, gifted debater with an interest in theatre.[49] He preferred playing larger-than-life characters in theatrical productions, which has influenced his political image
+            </p>
+        </section>
+        <section class="container mx-auto mt-8 px-4">
+            <h2 class="text-xl font-bold mb-2">Private life</h2>
+            <p>
+  Modi used to maintain a close and publicised relationship with his centenarian mother, Hiraben
+            </p>
+        </section>
+        <section class="container mx-auto mt-8 px-4">
+            <h2 class="text-xl font-bold mb-2">Early political career</h2>
+            <p>
+               In June 1975, Prime Minister Indira Gandhi declared a state of emergency in India that lasted until 1977. During this period, known as "the Emergency", many of her political opponents were jailed and opposition groups were banned.[88][89] Modi was appointed general secretary of the "Gujarat Lok Sangharsh Samiti", an RSS committee co-ordinating opposition to the Emergency in Gujarat. Shortly afterwards, the RSS was banned.[90] Modi was forced to go underground in Gujarat and frequently travelled in disguise to avoid arrest, once dressing as a monk and once as a Sikh.[91] He became involved in the printing of pamphlets opposing the government, sending them to Delhi and organising demonstrations.[92][93] He was also involved with creating a network of safe houses for individuals who were wanted by the government, and in raising funds for political refugees and activists.[94] During this period, Modi wrote a Gujarati-language book titled Sangharsh Ma Gujarat (In the Struggles of Gujarat), which describes events during the Emergency.[95][96] While in this role, Modi met trade unionist and socialist activist George Fernandes and several other national political figures
+            </p>
+        </section>
